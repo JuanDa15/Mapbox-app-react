42e4038113e253da131caf6bc7fbf55ecb5c1e96
Juan David Osorio Ortiz <jdoo1115@gmail.com>
2024-07-24 21:28:48 -0500

    SETTINGS

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mapbox-app-react/',
+  base: 'https://juanda15.github.io/mapbox-app-react/',
 })
